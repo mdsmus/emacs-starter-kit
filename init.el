b7449523df362f5f52ea04ebd55b547d27561763
Pedro Kroger <pedro.kroger@gmail.com>
2010-12-31 18:11:43 +0100

    Clean up init.el
    Remove port for emacs 22, add benchmarking of the init file, a few
    helper functions and clean-up a little.

--- init.el
+++ init.el
@@ -9,37 +9,45 @@
 ;; and brighter; it simply makes everything else vanish."
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 
+;; Benchmarking
+(defvar *benchmark-init-p* t)
+(defvar *emacs-load-start* (current-time))
+
 ;; Turn off mouse interface early in startup to avoid momentary display
 ;; You really don't need these; trust me.
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; Load path etc.
+;; Utilities functions
+(defun calc-startup-time ()
+  (flet ((sum-time (time) (apply #'+ (subseq time 0 2))))
+    (- (sum-time (current-time))
+       (sum-time *emacs-load-start*))))
 
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
+(defun dotemacs (&rest things)
+  "Concatenate a bunch of things to emacs' configuration directory. "
+  (apply #'concat dotfiles-dir things))
+
+;; Load path etc.
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (dotemacs "elpa-to-submit"))
+(setq autoload-file (dotemacs "loaddefs.el"))
+(setq package-user-dir (dotemacs "elpa"))
+(setq custom-file (dotemacs "custom.el"))
 
 ;; Load up ELPA, the package manager
-
-(add-to-list 'load-path dotfiles-dir)
-
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
-
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq package-user-dir (concat dotfiles-dir "elpa"))
-(setq custom-file (concat dotfiles-dir "custom.el"))
-
 (require 'package)
-(dolist (source '(("technomancy" . "http://repo.technomancy.us/emacs/")
-                  ("elpa" . "http://tromey.com/elpa/")))
-  (add-to-list 'package-archives source t))
+(setq elpa-source-list '(("technomancy" . "http://repo.technomancy.us/emacs/")
+                         ("elpa" . "http://tromey.com/elpa/")))
+
+(dolist (source elpa-source-list) (add-to-list 'package-archives source t))
 (package-initialize)
 (require 'starter-kit-elpa)
 
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
-
 (require 'cl)
 (require 'saveplace)
 (require 'ffap)
@@ -47,11 +55,7 @@
 (require 'ansi-color)
 (require 'recentf)
 
-;; backport some functionality to Emacs 22 if needed
-(require 'dominating-file)
-
 ;; Load up starter kit customizations
-
 (require 'starter-kit-defuns)
 (require 'starter-kit-bindings)
 (require 'starter-kit-misc)
@@ -62,18 +66,18 @@
 (require 'starter-kit-ruby)
 (require 'starter-kit-js)
 
-(regen-autoloads)
+(if (boundp 'regen-autoloads) (regen-autoloads))
 (load custom-file 'noerror)
 
-;; You can keep system- or user-specific customizations here
-(setq system-specific-config (concat dotfiles-dir system-name ".el")
-      user-specific-config (concat dotfiles-dir user-login-name ".el")
-      user-specific-dir (concat dotfiles-dir user-login-name))
-(add-to-list 'load-path user-specific-dir)
+;; You can keep system- or user-specific customizations here:
+(loop for name in (list system-name user-login-name)
+      for file = (dotemacs name ".el") do
+      (if (file-exists-p file) (load file)))
 
-(if (file-exists-p system-specific-config) (load system-specific-config))
-(if (file-exists-p user-specific-config) (load user-specific-config))
-(if (file-exists-p user-specific-dir)
-  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
+(let ((user-specific-dir (dotemacs user-login-name)))
+  (if (file-exists-p user-specific-dir)
+      (mapc #'load (directory-files user-specific-dir nil ".*el$"))))
 
-;;; init.el ends here
+;; Benchmarking
+(if *benchmark-init-p*
+    (message "My .emacs loaded in %ds" (calc-startup-time)))
